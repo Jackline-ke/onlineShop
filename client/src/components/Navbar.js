03b65145c12e9f58daebad7bdd223f46397627eb
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-27 17:14:05 +0300

    Added components and pages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between px-32 py-4'>
         <Link to='/'><h1>Home</h1></Link>
         <Link to='blog'><h1>Blog</h1></Link>
         <Link to='cart'><h1>Cart</h1></Link>
